8e086d57ae314fe3c3daa225ac443068cb9ea1fa
T.Yong <yangtao@showboom.cn>
2019-05-14 17:43:23 +0800

    新增目录

---
+++ config/index.js
@@ -0,0 +1,13 @@
+//  应用配置
+const config = {
+  //  接口地址前缀
+  baseURL: '',
+  //  外部图片地址前缀
+  baseImgURL: '',
+  //  外部网页地址前缀
+  baseWebURL: '',
+  //  腾讯地图密钥
+  QQMapWXKey: 'HVOBZ-WJCKX-O534S-TXDEZ-MEXO3-XTBEX'
+}
+
+module.exports = config;
---
+++ utils/api.js
@@ -0,0 +1,133 @@
+const config = require('./../config/index');
+const QQMapWX = require('./../lib/qqmap-wx-jssdk.min');
+
+//  绑定
+let bindAPI = (apiName, bindObj = wx) => (o = {}) => new Promise((resolve, reject) => {
+  bindObj[apiName](Object.assign({}, o, {
+    success: resolve,
+    fail: reject
+  }))
+});
+
+//  接口的命名空间
+let apiSpace = {
+  //  网络
+  net: [
+    'request',
+    'uploadFile',
+    'downloadFile',
+    'connectSocket',
+    'sendSocketMessage',
+    'closeSocket',
+    'onSocketOpen',
+    'onSocketClose',
+    'onSocketMessage',
+    'onSocketError'
+  ],
+  //  数据缓存
+  dataCache: [
+    'getStorage',
+    'getStorageSync',
+    'setStorage',
+    'setStorageSync',
+    'removeStorage',
+    'removeStorageSync',
+    'clearStorage',
+    'clearStorageSync',
+    'getStorageInfo',
+    'getStorageInfoSync'
+  ],
+  //  位置
+  location: [
+    'getLocation',
+    'openLocation',
+    'chooseLocation'
+  ],
+  //  设备
+  device: [
+    //  网络
+    'getNetworkType',
+    //  电话
+    'makePhoneCall',
+    //  扫码
+    'scanCode'
+  ],
+  //  界面
+  userface: [
+    //  交互
+    'showToast',
+    'hideToast',
+    'showLoading',
+    'hideLoading',
+    'showModal',
+    'showActionSheet',
+    //  下拉刷新
+    'stopPullDownRefresh'
+  ],
+  //  开放接口
+  openAPI: [
+    //  授权
+    'authorize',
+    //  登录
+    'login',
+    'checkSession',
+    //  支付
+    'requestPayment',
+    //  设置
+    'getSetting',
+    'openSetting',
+    //  用户信息
+    'getUserInfo'
+  ],
+  //  地图
+  map: [
+    'createMapContext',
+    'moveToLocation',
+    'translateMarker'
+  ],
+  //  路由
+  route: [
+    'navigateTo',
+    'navigateBack',
+    'redirectTo',
+    'switchTab',
+    'reLaunch'
+  ]
+}
+
+let rawNameArr = [];
+
+for (let key in apiSpace) {
+  rawNameArr = [...rawNameArr, ...apiSpace[key]]
+}
+
+const apis = rawNameArr.reduce((accu, elt) => {
+  if (Object.prototype.toString.call(elt) === '[object String]') {
+    accu[elt] = bindAPI(elt)
+  } else {
+    // elt.names.forEach(name=>{
+    //   accu[name] = bindAPI(elt.name, elt.thisArg)
+    // })
+    accu[elt.name] = bindAPI(elt.name, elt.thisArg)
+  }
+  return accu;
+}, {});
+
+// qqwx 地图 api
+apis.createQQMap = () => {
+  let ins = new QQMapWX({ key: config.QQMapWXKey });
+  return [
+    'search',
+    'getSuggestion',
+    'reverseGeocoder',
+    'geocoder',
+    'getCityList',
+    'getDistrictByCityId',
+    'calculateDistance'
+  ].reduce((accu, name) => {
+    accu[name] = bindAPI(name, ins);
+    return accu;
+  }, {});
+};
+
+module.exports = apis;

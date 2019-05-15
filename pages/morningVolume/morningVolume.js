b2ed08d52e07ae97a8ee5dd82fb4cb84aaa06ebb
yangtao <yangtao@showboom.cn>
2019-05-15 09:58:00 +0800

    新增轮播 酒店服务

--- pages/morningVolume/morningVolume.js
+++ pages/morningVolume/morningVolume.js
@@ -1,18 +1,11 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/morningVolume/morningVolume.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    imgUrls: [
-      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-    ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 1000
+
   },
 
   /**

9791f08e0ba661f2485d4a4a4dae420451a2634d
Jinjiyong <80239214@qq.com>
2019-05-15 19:00:13 +0800

    booking

---
+++ pages/bookingOrder/bookingOrder.js
@@ -0,0 +1,37 @@
+Page({
+  data: {
+    //  房间数集合索引
+    roomNumberIndex: 0,
+    //  房间数集合
+    roomNumberArray: [1,2,3,4,5,6,7,8,9,10],
+    //  房间数
+    roomNumber: 1,
+    //  预计办理入住时间起始值
+    startCheckInTime: new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate(),
+    //  预计办理入住时间
+    checkInTime: '',
+    //  是否显示金额明细
+    isSumDetail: false
+  },
+  //
+  bindRoomNumberChange: function (e) {
+    this.setData({
+      roomNumberIndex: e.detail.value,
+      roomNumber: this.data.roomNumberArray[e.detail.value]
+    });
+    console.log(e.detail.value);
+    console.log(this.data.roomNumber);
+  },
+  //  选择预计办理入住时间
+  bindDateChange: function (e) {
+    this.setData({
+      checkInTime: e.detail.value
+    });
+  },
+  //  显示/隐藏金额明细
+  toggleSumDetail: function () {
+    this.setData({
+      isSumDetail: !this.data.isSumDetail
+    });
+  }
+})

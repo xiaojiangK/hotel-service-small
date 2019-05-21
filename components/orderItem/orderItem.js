e3698d1c574b946b31bd5ebd758af4c1c164a945
leftYang <876900309@qq.com>
2019-05-21 11:29:48 +0800

    '新增订单倒计时'

--- components/orderItem/orderItem.js
+++ components/orderItem/orderItem.js
@@ -1,4 +1,5 @@
 // components/orderItem/orderItem.js
+import { resetTime } from '../../utils/common.js'
 Component({
   /**
    * 组件的属性列表
@@ -7,17 +8,27 @@ Component({
 
   },
 
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached() { 
+      this.startCountDown()
+    },
+    moved() { },
+    detached() { },
+  },
   /**
    * 组件的初始数据
    */
   data: {
-
+    remainTime:''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    startCountDown(){
+      resetTime(130,this)
+    }
   }
 })

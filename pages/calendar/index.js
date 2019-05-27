14ef16edf27318a43fa14226f2ecfd4f8462fffe
yangtao <yangtao@showboom.cn>
2019-05-27 09:44:51 +0800

    日历

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -15,118 +15,99 @@ Page({
     markcheckOutDate: false,   //标记结束时间是否已经选择
     sFtv: [
       {
-        month:1,
-        day:1,
-        name:"1"
-        // name:"元旦"
+        month: 1,
+        day: 1,
+        name: "1"
       },
       {
         month: 2,
         day: 14,
         name: "14"
-        // name: "情人节"
       },
       {
         month: 3,
         day: 8,
         name: "8"
-        // name: "妇女节"
       },
       {
         month: 3,
         day: 12,
         name: "12"
-        // name: "植树节"
       },
       {
         month: 3,
         day: 15,
         name: "15"
-        // name: "消费者权益日"
       },
       {
         month: 4,
         day: 1,
         name: "1"
-        // name: "愚人节"
       },
       {
         month: 5,
         day: 1,
         name: "1"
-        // name: "劳动节"
       },
       {
         month: 5,
         day: 4,
         name: "4"
-        // name: "青年节"
       },
       {
         month: 5,
         day: 12,
         name: "12"
-        // name: "护士节"
       },
       {
         month: 6,
         day: 1,
         name: "1"
-        // name: "儿童节"
       },
       {
         month: 7,
         day: 1,
         name: "1"
-        // name: "建党节"
       },
       {
         month: 8,
         day: 1,
         name: "1"
-        // name: "建军节"
       },
       {
         month: 9,
         day: 10,
         name: "10"
-        // name: "教师节"
       },
       {
         month: 9,
         day: 28,
         name: "28"
-        // name: "孔子诞辰"
       },
       {
         month: 10,
         day: 1,
         name: "1"
-        // name: "国庆节"
       },
       {
         month: 10,
         day: 6,
         name: "6"
-        // name: "老人节"
       },
       {
         month: 10,
         day: 24,
         name: "24"
-        // name: "联合国日"
       },
       {
         month: 12,
         day: 24,
         name: "24"
-        // name: "平安夜"
       },
       {
         month: 12,
         day: 25,
         name: "25"
-        // name: "圣诞节"
       }
     ]
   },
@@ -268,17 +249,17 @@ Page({
       dateList.push(dateItem);
     }
     var sFtv = this.data.sFtv;
-    for (let i = 0; i < dateList.length; i++){//加入公历节日
-       for(let k = 0; k < sFtv.length; k++){
-         if (dateList[i].month == sFtv[k].month){
-           let days = dateList[i].days;
-           for (let j = 0; j < days.length; j++){
-             if (days[j].day == sFtv[k].day){
-               days[j].daytext = sFtv[k].name
-             }
-           }
-         }
-       }
+    for (let i = 0; i < dateList.length; i++) {//加入公历节日
+      for (let k = 0; k < sFtv.length; k++) {
+        if (dateList[i].month == sFtv[k].month) {
+          let days = dateList[i].days;
+          for (let j = 0; j < days.length; j++) {
+            if (days[j].day == sFtv[k].day) {
+              days[j].daytext = sFtv[k].name
+            }
+          }
+        }
+      }
     }
     this.setData({
       dateList: dateList

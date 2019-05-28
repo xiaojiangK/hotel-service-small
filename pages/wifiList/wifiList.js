408f877e6351e160d60ed0e07bf0b60d382c5b82
yangtao <yangtao@showboom.cn>
2019-05-28 17:31:27 +0800

    addwifi2.0

--- pages/wifiList/wifiList.js
+++ pages/wifiList/wifiList.js
@@ -11,10 +11,23 @@ Page({
     this.loadData();
   },
   loadData() {
-    //取json里wifi名字
-    this.setData ({
-      list: wifiData.wifiJsonList.data.a.wifi
-    })
+    const data = wifiData.wifiJsonList.data.a.wifi
+    let list = data.sort(this.compare("ssid"))
+    this.setData ({ list })
+  },
+  //房间号升序排列
+  compare: function(propertyName) {
+    return function(object1, object2) {
+        var value1 = object1[propertyName];
+        var value2 = object2[propertyName];
+        if(value2 > value1) {
+            return -1;
+        } else if(value2 < value1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
   },
   connectWifi: function (e) {
     const that = this;

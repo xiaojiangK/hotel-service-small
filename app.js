39f7274ef68b6f2714aee575da0206b18e4ab715
leftYang <876900309@qq.com>
2019-05-28 12:07:45 +0800

    '增加分享功能'

--- app.js
+++ app.js
@@ -179,5 +179,15 @@ App({
     shopCar:[],
     newArr:[],
     scene: null
+  },
+  onShareAppMessage: function (res) {
+    // if (res.from === 'button') {
+    //   // 来自页面内转发按钮
+    //   console.log(res.target)
+    // }
+    return {
+      title: '自定义转发标题',
+      path: '/page/user?id=123'
+    }
   }
 })
